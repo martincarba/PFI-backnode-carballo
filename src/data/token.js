65cc03689dc690f94087f061a24de6fc71edbf83
Martin Carballo <martin3sq3carballo@gmail.com>
2025-12-16 01:30:59 -0300

    fix

--- src/data/token.js
+++ src/data/token.js
@@ -1,11 +1,15 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
-const secret_key = process.env.JWT_SECRET_KEY || "lsfm398fnsfj2Ar3q298";
-console.log(secret_key)
+const secret_key = process.env.JWT_SECRET || "lsfm398fnsfj2Ar3q298";
 
 export const generateToken = (userData) => {
-  const user = {id: userData.id, email: userData.email};
-  const expiration = { expiresIn: '1h' };
+  const user = {
+    id: userData.id,
+    email: userData.email
+  };
+
+  const expiration = { expiresIn: "1h" };
+
   return jwt.sign(user, secret_key, expiration);
-}
+};
